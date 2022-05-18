ce85474fd5a7f325239e96b6e8817a83dc47be69
Cris Moen <cris.moen@runtosolve.com>
2022-05-18 16:49:31 -0400

    clean up plotting example

--- test/TopHat_1_span_plots.jl
+++ test/TopHat_1_span_plots.jl
@@ -46,40 +46,8 @@ UI.plot_purlin_geometry(purlin_line.inputs.cross_section_dimensions[1][2], purli
 
 UI.plot_top_hat_purlin_geometry(top_hat_purlin_line.inputs.top_hat_cross_section_dimensions[1][1], top_hat_purlin_line.purlin_cross_section_data[1].node_geometry[:,1], top_hat_purlin_line.purlin_cross_section_data[1].node_geometry[:,2], roof_slope, top_hat_purlin_line.top_hat_purlin_cross_section_data[1].node_geometry[:,1], top_hat_purlin_line.top_hat_purlin_cross_section_data[1].node_geometry[:,2], top_hat_purlin_line.top_hat_cross_section_data[1].node_geometry[:,1], top_hat_purlin_line.top_hat_cross_section_data[1].node_geometry[:,2])
 
-
-plot_net_section_top_hat_purlin_geometry(t, xcoords_center_purlin, ycoords_center_purlin, roof_slope, xcoords_center_top_hat_purlin, ycoords_center_top_hat_purlin, net_top_hat_purlin_node_geometry)
-
 UI.plot_net_section_top_hat_purlin_geometry(purlin_line.inputs.cross_section_dimensions[1][2], top_hat_purlin_line.purlin_cross_section_data[1].node_geometry[:,1], top_hat_purlin_line.purlin_cross_section_data[1].node_geometry[:,2], top_hat_purlin_line.top_hat_cross_section_data[1].node_geometry[:,1], top_hat_purlin_line.top_hat_cross_section_data[1].node_geometry[:,2], roof_slope, top_hat_purlin_line.top_hat_purlin_cross_section_data[1].node_geometry[:,1],  top_hat_purlin_line.top_hat_purlin_cross_section_data[1].node_geometry[:,2], top_hat_purlin_line.top_hat_purlin_net_cross_section_data[1].node_geometry)
 
 
 
 
-
-
-
-
-
-
-center_nodes = [xcoords_center ycoords_center zeros(Float64, length(xcoords_center))]
-
-center_nodes_rotated = Geometry.rotate_nodes(center_nodes, rotation_axis = "z", rotation_center = [0.0, 0.0, 0.0], θ=atan(roof_slope))
-
-    #Calculate surface normals.
-closed_or_open = 1
-unitnormals = SectionProperties.surface_normals(xcoords_center, ycoords_center, closed_or_open)
-
-
-nodenormals = SectionProperties.avg_node_normals(unitnormals, closed_or_open)
-
-
-xcoords_out, ycoords_out = SectionProperties.xycoords_along_normal(xcoords_center, ycoords_center, nodenormals, t/2)
-
-out_nodes = [xcoords_out ycoords_out zeros(Float64, length(xcoords_center))]
-
-out_nodes_rotated = Geometry.rotate_nodes(out_nodes, rotation_axis = "z", rotation_center = [0.0, 0.0, 0.0], θ=atan(roof_slope))
-
-xcoords_in, ycoords_in = SectionProperties.xycoords_along_normal(xcoords_center, ycoords_center, nodenormals, -t/2)
-
-in_nodes = [xcoords_in ycoords_in zeros(Float64, length(xcoords_center))]
-
-in_nodes_rotated = Geometry.rotate_nodes(in_nodes, rotation_axis = "z", rotation_center = [0.0, 0.0, 0.0], θ=atan(roof_slope))
